7ab5e014ec8e025cfcb01acd3cfbb91c5ccd595c
Noble-01 <zaccripps5@gmail.com>
2020-03-17 19:18:08 -0700

    comments to swift model

--- Milestone1/Model/Potato.swift
+++ Milestone1/Model/Potato.swift
@@ -8,15 +8,23 @@
 //the struct contains all the parameters that make for the objects in "Potato"
 import Foundation
 struct Potato : Identifiable{
+    ///A class of types whose instances hold the value of an entity with stable identity
+    ///id is given to identify the objects of potatos in the array
     var id = UUID()
+    ///generic name for potato
     let name:String
+    ///scientific name for potato
     let scienceName:String
+    ///type of family for potato
     let family:String
+    ///weight of the potato
     let weight: String
+    ///nutrition values of potato
     let nutrition:String
+    ///image name for potato in the assets folder
     let image:String
 
-    
+    ///initialize the attributes of the class
     init(name:String, family:String, weight:String, scienceName:String, nutrition:String, image: String) {
         self.name = name
         self.family  = family
